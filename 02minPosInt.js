38280fc9fca8d453abf7a7ad7505bd8dc0c56142
Martun <65818128+martun-avagyan@users.noreply.github.com>
2022-02-20 18:17:53 +0400

    fix: 02 add the case for no positive integers

--- 02minPosInt.js
+++ 02minPosInt.js
@@ -4,6 +4,7 @@ const minPosInt = function (arr, i = 0, toReturn = arr[0]) {
   i++;
 
   if (i > arr.length) {
+    toReturn = toReturn > 0 ? toReturn : -1;
     return toReturn;
   } else {
     toReturn = (arr[i] < toReturn) & (arr[i] > 0) ? arr[i] : toReturn;
@@ -12,3 +13,4 @@ const minPosInt = function (arr, i = 0, toReturn = arr[0]) {
 };
 
 console.log(minPosInt([10, 55, 43, 2, 11, 7, -5, 1])); // 1
+console.log(minPosInt([-5, -9, -111, -1000, -7])); // -1
