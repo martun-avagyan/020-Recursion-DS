d0eb7b7a8e9c9a998e9999c0b8ffae7827702079
Martun <65818128+martun-avagyan@users.noreply.github.com>
2022-02-20 18:14:43 +0400

    feat: 02 add function to get the minimum positive integer

--- 02minPosInt.js
+++ 02minPosInt.js
@@ -1,3 +1,14 @@
 "use strict";
-let i = -1;
-const minPosInt = function (arr) {};
+
+const minPosInt = function (arr, i = 0, toReturn = arr[0]) {
+  i++;
+
+  if (i > arr.length) {
+    return toReturn;
+  } else {
+    toReturn = (arr[i] < toReturn) & (arr[i] > 0) ? arr[i] : toReturn;
+    return minPosInt(arr, i, toReturn);
+  }
+};
+
+console.log(minPosInt([10, 55, 43, 2, 11, 7, -5, 1])); // 1
